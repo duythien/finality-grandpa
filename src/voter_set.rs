0f571bbc42e3302945a7679475d76e64e42aac0a
Thien Tran <fcduythien@gmail.com>
2023-03-14 13:17:37 +0700

    debug

--- src/voter_set.rs
+++ src/voter_set.rs
@@ -126,6 +126,7 @@ impl<Id: Eq + Ord + Debug> VoterSet<Id> {
 	/// Get the nth voter in the set, modulo the size of the set,
 	/// as per the associated total order.
 	pub fn nth_mod(&self, n: usize) -> (&Id, &VoterInfo) {
+		println!("-----debug n: {:?} and len {:?}", n, self.voters.len());
 		let nth = self.nth(n % self.voters.len());
 		match nth {
 			Some(nth) => nth,
